f63ca30fb30ea82228a3b0c993a92b1cf95ed8d6
Wivor <wiktor756@gmail.com>
2019-11-15 16:57:48 +0100

    Added ScriptableObject for rat and abstract Ability

---
+++ Rotten Zone/Assets/Scripts/Rat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName ="Rat")]
+public class Rat : ScriptableObject
+{
+    public string unitName;
+    public int healt;
+    public int attack;
+    public int defence;
+    public int speed;
+    public int cost;
+
+    public AudioClip attackClip;
+    public AudioClip deathClip;
+
+    public List<Ability> abilities;
+}
