efdd422c9b4725dbb56ef2d9265a64516ee56b18
Adven97 <atomczak30@gmail.com>
2019-11-19 22:28:58 +0100

    veery basic game menu

---
+++ Rotten Zone/Assets/Scripts/Menu/PlayButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void handlePlayButton(){
+        //add scene when its ready
+        //SceneManager.LoadScene("sceneID")
+
+        //load next level in queue
+       //SceneManager.LoadScene(SceneManager.getActiveScene().buildIndex +1)
+    }
+}
