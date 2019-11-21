55192cb8120351221c224854fdfc63b3164bfa22
Wiktor <wiktor756@gmail.com>
2019-11-21 15:16:59 +0100

    animation asigned to current object close #25

--- Rotten Zone/Assets/Scripts/Rat.cs
+++ Rotten Zone/Assets/Scripts/Rat.cs
@@ -8,19 +8,8 @@ public class Rat : MonoBehaviour
 
     void Start()
     {
-        // załadowanie DragonBonesData do fabryki
         UnityFactory.factory.LoadData(scriptableRat.dragonBonesData);
-
-        // przypisanie komponentu aktualnego obiektu do zmiennej
-        // armatureComponent = GetComponent<UnityArmatureComponent>();
-
-        // przypisanie DragonBonesData do komponentu aktualnego obiektu
-        // armatureComponent.unityData = scriptableRat.dragonBonesData;
-        
-        // stworzenie nowego obiektu z fabryki
-        armatureComponent = UnityFactory.factory.BuildArmatureComponent("Armature");
-        
-        Debug.Log(armatureComponent.animation);
+        armatureComponent = UnityFactory.factory.BuildArmatureComponent("Armature", gameObject: gameObject);
         armatureComponent.animation.Play("walking");
     }
 }
