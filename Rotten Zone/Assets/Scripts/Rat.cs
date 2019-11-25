9778831d0f4f0ddc692710f82628d4018923d772
Wiktor <wiktor756@gmail.com>
2019-11-25 23:11:09 +0100

    Modified rat prefab

--- Rotten Zone/Assets/Scripts/Rat.cs
+++ Rotten Zone/Assets/Scripts/Rat.cs
@@ -1,15 +1,19 @@
 ﻿using DragonBones;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Rat : MonoBehaviour
 {
+    public NavMeshAgent agent;
     public ScriptableRat scriptableRat;
     UnityArmatureComponent armatureComponent;
 
     void Start()
     {
         UnityFactory.factory.LoadData(scriptableRat.dragonBonesData);
-        armatureComponent = UnityFactory.factory.BuildArmatureComponent("Armature", gameObject: gameObject);
+        armatureComponent = UnityFactory.factory.BuildArmatureComponent("Armature", gameObject: transform.GetChild(0).gameObject);
         armatureComponent.animation.Play("walking");
+
+        //agent.SetDestination();
     }
 }
