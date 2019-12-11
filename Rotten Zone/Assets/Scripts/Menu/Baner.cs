8ddc213c0f1c2841cac9f7823242ffb007593e70
209486 <209486@CARBON.CTI.P.LODZ.PL>
2019-12-11 19:37:35 +0100

    add baner

---
+++ Rotten Zone/Assets/Scripts/Menu/Baner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+public class Baner : MonoBehaviour
+{
+     public string gameId = "3386462";
+    public string placementId = "bannerPlacement";
+    public bool testMode = true;
+
+    void Start () {
+        Advertisement.Initialize (gameId, testMode);
+        StartCoroutine (ShowBannerWhenReady ());
+    }
+
+    IEnumerator ShowBannerWhenReady () {
+        while (!Advertisement.IsReady (placementId)) {
+            yield return new WaitForSeconds (0.5f);
+        }
+        Advertisement.Banner.Show (placementId);
+    }
+}
