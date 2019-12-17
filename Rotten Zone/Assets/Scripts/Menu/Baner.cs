73542538389c95c78d40b6dccae3006afb21538d
Adven97 <atomczak30@gmail.com>
2019-12-18 00:58:54 +0100

    add simple popup

--- Rotten Zone/Assets/Scripts/Menu/Baner.cs
+++ Rotten Zone/Assets/Scripts/Menu/Baner.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Advertisements;
+using UnityEngine.Monetization;
+
 public class Baner : MonoBehaviour
 {
      public string gameId = "3386462";
-    public string placementId = "bannerPlacement";
+    public string placementId = "rottenBanner";
     public bool testMode = true;
 
     void Start () {
+        Monetization.Initialize (gameId,testMode);
         Advertisement.Initialize (gameId, testMode);
         StartCoroutine (ShowBannerWhenReady ());
     }
 
     IEnumerator ShowBannerWhenReady () {
         while (!Advertisement.IsReady (placementId)) {
-            yield return new WaitForSeconds (0.5f);
+            yield return new WaitForSeconds (2.5f);
         }
+        // Advertisement.Banner.SetPosition (BannerPosition.TOP_CENTER);
         Advertisement.Banner.Show (placementId);
     }
 }
---
+++ Rotten Zone/Assets/Scripts/Menu/PopupOpener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupOpener : MonoBehaviour
+{
+    public GameObject popup;
+
+    public void openPopup(){
+        if(popup != null)
+        {
+            popup.SetActive(true);
+        }
+    }
+
+    public void closePopup(){
+        if(popup != null)
+        {
+            popup.SetActive(false);
+        }
+    }
+}
+
