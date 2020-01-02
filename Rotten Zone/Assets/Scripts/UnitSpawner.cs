8fd18374e1f12c672d2b9bb0f8a25f57c87325db
Wiktor <wiktor756@gmail.com>
2020-01-02 14:51:47 +0100

    Swaped lanes to spawn correctly

--- Rotten Zone/Assets/Scripts/UnitSpawner.cs
+++ Rotten Zone/Assets/Scripts/UnitSpawner.cs
@@ -27,10 +27,10 @@ public class UnitSpawner : MonoBehaviour
         switch (laneID)
         {
             case 9:
-                GameObject rat1 = Instantiate(ratPrefab);
-                rat1.GetComponent<Rat>().Initialize(rats[currentRat]);
-                rat1.transform.position = GetComponent<GameManager>().pathOne[0].transform.position;
-                rat1.GetComponent<NavMeshAgent>().agentTypeID = NavMesh.GetSettingsByIndex(2).agentTypeID;
+                GameObject rat3 = Instantiate(ratPrefab);
+                rat3.GetComponent<Rat>().Initialize(rats[currentRat]);
+                rat3.transform.position = GetComponent<GameManager>().pathThree[0].transform.position;
+                rat3.GetComponent<NavMeshAgent>().agentTypeID = int.Parse(NavMesh.GetSettingsNameFromID(0));
                 break;
             case 10:
                 GameObject rat2 = Instantiate(ratPrefab);
@@ -39,10 +39,10 @@ public class UnitSpawner : MonoBehaviour
                 rat2.GetComponent<NavMeshAgent>().agentTypeID = NavMesh.GetSettingsByIndex(1).agentTypeID;
                 break;
             case 11:
-                GameObject rat3 = Instantiate(ratPrefab);
-                rat3.GetComponent<Rat>().Initialize(rats[currentRat]);
-                rat3.transform.position = GetComponent<GameManager>().pathThree[0].transform.position;
-                rat3.GetComponent<NavMeshAgent>().agentTypeID = int.Parse(NavMesh.GetSettingsNameFromID(0));
+                GameObject rat1 = Instantiate(ratPrefab);
+                rat1.GetComponent<Rat>().Initialize(rats[currentRat]);
+                rat1.transform.position = GetComponent<GameManager>().pathOne[0].transform.position;
+                rat1.GetComponent<NavMeshAgent>().agentTypeID = NavMesh.GetSettingsByIndex(2).agentTypeID;
                 break;
         }
     }
