fd22895dfe9e7c2371a60decea6308c629cd474a
Łukasz Malina <lukasz662@gmail.com>
2020-01-08 19:28:54 +0100

    Added scriptable projectile object

---
+++ Rotten Zone/Assets/Scripts/ScriptableObjects/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : ScriptableObject
+{
+    public int velocity;
+    public int damage;
+
+    public AudioClip shotClip;
+
+    public abstract void onHit();
+}
