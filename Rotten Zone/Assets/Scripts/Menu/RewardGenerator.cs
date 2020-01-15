4ca8fd60bbcfe31e7bc1ac1a27d2791d6b7fce75
209486 <209486@CARBON.CTI.P.LODZ.PL>
2020-01-15 19:36:04 +0100

    add reward modal

---
+++ Rotten Zone/Assets/Scripts/Menu/RewardGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardGenerator : MonoBehaviour
+{
+    public GameObject modal;
+    public Text txt;
+    public void getAward()
+    {
+        modal.SetActive(true);
+        System.Random random = new System.Random();
+        int randomAward= random.Next(1, 30);
+
+        txt = gameObject.GetComponent<Text>();
+        txt.text = "Score : " + randomAward;
+    }
+
+}
