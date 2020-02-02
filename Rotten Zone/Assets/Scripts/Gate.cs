8e3410d191cf7bc65647911a4257a5b0f266546e
Wiktor <wiktor756@gmail.com>
2020-02-02 23:02:20 +0100

    Added conditions for game ending

--- Rotten Zone/Assets/Scripts/Gate.cs
+++ Rotten Zone/Assets/Scripts/Gate.cs
@@ -1,6 +1,7 @@
 ﻿public class Gate : AttackableObject
 {
     GameManager gameManager;
+    bool gameEnded = false;
 
     private void Start()
     {
@@ -10,13 +11,36 @@
 
     public override void DealDamage(int damage)
     {
+        if (gameEnded)
+            return;
+
         if (team == Team.A)
         {
             gameManager.baseAhealth -= damage;
+            if (gameManager.baseAhealth <= 0)
+            {
+                gameEnded = true;
+                PlayerWon();
+            }
         }
         else
         {
             gameManager.baseBhealth -= damage;
+            if (gameManager.baseBhealth <= 0)
+            {
+                gameEnded = true;
+                PlayerLost();
+            }
         }
+    }
+
+    private void PlayerWon()
+    {
+
+    }
+
+    private void PlayerLost()
+    {
+
     }
 }
